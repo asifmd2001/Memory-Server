22404e44e36110800905d580714a828d29e85826
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2022-02-05 09:21:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.use('/',(req,res)=>{
+  res.json("api");
+})
+
+const CONNECTION_URL = 'mongodb+srv://asifmd:123123123@cluster0.ykzsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
