7be5c5a38ae0387462769dca1f84d48058c261d4
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2022-02-09 16:57:04 +0000

    auth

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 
@@ -13,16 +14,16 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+app.use('/user',userRoutes)
 app.use('/',(req,res)=>{
   res.json("api");
 })
 
 const CONNECTION_URL = 'mongodb+srv://asifmd:123123123@cluster0.ykzsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 5040;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
