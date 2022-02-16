303861a722ad1626fc35c9ebd363828bcf568cbf
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2022-02-16 14:00:43 +0000

    .

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import env from "dotenv";
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
 const app = express();
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+env.config();
 app.use('/posts', postRoutes);
 app.use('/user',userRoutes)
 app.use('/',(req,res)=>{
